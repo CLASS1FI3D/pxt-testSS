b37dff9e27d294fffe5ac33ba2e52ca1249dcfda
CLASS1FI3D <48974547+CLASS1FI3D@users.noreply.github.com>
2019-07-18 11:30:26 +1000

    gui

--- main.ts
+++ main.ts
@@ -5,23 +5,23 @@
 namespace pxt_testSS {
     let Potentiometer = 0
     let mode = 1
-    basic.showNumber(mode)   
+    basic.showNumber(mode)
     //% block="Slim Circuit 1 || - Pot %potPin, LED %ledPin, LDR %ldrPin"
     //% expandableArgumentMode="toggle"    
     export function Slim_Circuit(potPin: AnalogPin, ledPin: AnalogPin, ldrPin: AnalogPin) {
-                
+
         input.onButtonPressed(Button.A, function () {
             mode += 1
             if (mode >= 3) {
                 mode = 1
-            }   
-            basic.showNumber(mode)         
+            }
+            basic.showNumber(mode)
         })
 
         switch (mode) {
-            case 1: 
+            case 1:
                 Potentiometer = pins.analogReadPin(potPin)
-                pins.analogWritePin(ledPin, Potentiometer)
+                pins.analogWritePin(ledPin, 356)
                 break;
             case 2:
                 Potentiometer = pins.analogReadPin(ldrPin)
@@ -31,5 +31,5 @@ namespace pxt_testSS {
                 break;
         }
     }
-    
+
 }
