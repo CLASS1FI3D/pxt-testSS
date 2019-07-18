b3c076a9f2db5f72b9d5e9e98ea3e6027f7f19fb
CLASS1FI3D <48974547+CLASS1FI3D@users.noreply.github.com>
2019-07-18 11:10:59 +1000

    asdf

--- main.ts
+++ main.ts
@@ -4,23 +4,9 @@
 //% color="#FF9C18"
 namespace pxt_testSS {
 
-    /**
-     * This is a statement block
-     */
-    //% block
-    export function buttonChecker() {
-        let mode = 0
-        input.onButtonPressed(Button.A, function () {
-            if (mode < 1) {
-                mode += 1
-            } else {
-                mode = 0
-            }
-        })
-    }
-
-    //% block
-    export function Slim_Circuit_2() {
+    //% block="Slim Circuit 1 || Input %potPin, Output %ledPin"
+    //% expandableArgumentMode="toggle"    
+    export function Slim_Circuit(potPin: AnalogPin, ledPin: AnalogPin) {
         let Potentiometer = 0
         let mode = 0
 
@@ -34,43 +20,13 @@ namespace pxt_testSS {
         })
 
         if (mode == 0) {
-            Potentiometer = pins.analogReadPin(AnalogPin.P0)
-            pins.analogWritePin(AnalogPin.P0, Potentiometer)
+            Potentiometer = pins.analogReadPin(potPin)
+            pins.analogWritePin(ledPin, Potentiometer)
         } else {
             Potentiometer = pins.analogReadPin(AnalogPin.P1)
             pins.analogWritePin(AnalogPin.P0, Potentiometer)
         }
     }
 
-    /**
-     * This is a statement block with a parameter
-     */
-    //% block
-    export function move(steps: number) {
-
-    }
-
-    /**
-     * This is a reporter block that returns a number
-     */
-    //% block
-    export function randomNumber(): number {
-        return 0;
-    }
-
-    /**
-     * This is a reporter block that returns a boolean
-     */
-    //% block
-    export function randomBoolean(): boolean {
-        return false;
-    }
-
-    /**
-     * This is an event handler block
-     */
-    //% block="on event"
-    export function onEvent(handler: () => void) {
-
-    }
+    
 }
