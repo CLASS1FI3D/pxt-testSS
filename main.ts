ce806a89927e9caa8072d1e52068025e53517871
CLASS1FI3D <48974547+CLASS1FI3D@users.noreply.github.com>
2019-07-18 11:20:02 +1000

    asdf

--- main.ts
+++ main.ts
@@ -4,28 +4,31 @@
 //% color="#FF9C18"
 namespace pxt_testSS {
     let Potentiometer = 0
-    let mode = 0
-    //% block="Slim Circuit 1 || Input %potPin, Output %ledPin"
+    let mode = 1
+    //% block="Slim Circuit 1 || - Pot %potPin, LED %ledPin, LDR %ldrPin"
     //% expandableArgumentMode="toggle"    
-    export function Slim_Circuit(potPin: AnalogPin, ledPin: AnalogPin) {
-        
-        
+    export function Slim_Circuit(potPin: AnalogPin, ledPin: AnalogPin, ldrPin: AnalogPin) {
+                
         input.onButtonPressed(Button.A, function () {
             mode += 1
-            if (mode >= 2) {
-                mode = 0
+            if (mode >= 3) {
+                mode = 1
             }   
             basic.showNumber(mode)         
         })
 
-        if (mode == 0) {
-            Potentiometer = pins.analogReadPin(potPin)
-            pins.analogWritePin(ledPin, Potentiometer)
-        } else {
-            Potentiometer = pins.analogReadPin(AnalogPin.P1)
-            pins.analogWritePin(AnalogPin.P0, Potentiometer)
+        switch (mode) {
+            case 1: 
+                Potentiometer = pins.analogReadPin(potPin)
+                pins.analogWritePin(ledPin, Potentiometer)
+                break;
+            case 2:
+                Potentiometer = pins.analogReadPin(ldrPin)
+                pins.analogWritePin(ledPin, Potentiometer)
+                break;
+            default:
+                break;
         }
     }
-
     
 }
