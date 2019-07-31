2fbb043ddbef7832982e9fa2b050f1cbb8017837
CLASS1FI3D <48974547+CLASS1FI3D@users.noreply.github.com>
2019-08-01 08:53:08 +1000

    Updates.

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-// Here are the different types of blocks
-// supported by MakeCode
+// Makerverse
+
+/* Special Notes:
+ * Code bases forked
+ * Sonar - https://github.com/microsoft/pxt-sonar
+*/
+
+// Enum for the Sonar Units
+enum PingUnit {
+    //% block="μs"
+    MicroSeconds,
+    //% block="cm"
+    Centimeters,
+    //% block="inches"
+    Inches
+}
+
 
 //% color="#FF9C18"
 namespace pxt_testSS {
@@ -11,13 +26,7 @@ namespace pxt_testSS {
     //% expandableArgumentMode="toggle"    
     export function Circuit_1(potPin: AnalogPin, ledPin: AnalogPin, ldrPin: AnalogPin) {
 
-        input.onButtonPressed(Button.A, function () {
-            mode += 1
-            if (mode >= 4) {
-                mode = 1
-            }
-            basic.showNumber(mode)
-        })
+        checkMode(4);
 
         // Which mode are we in??
         switch (mode) {
@@ -41,9 +50,48 @@ namespace pxt_testSS {
                 break;
         }
     }
-    //% block="Slim Circuit 2 || - Pot %potPin, LED %ledPin, LDR %ldrPin"
+    //% block="Slim Circuit 2 || - Sonar Trigger %sonarTrigPin, Sonar Echo %sonarEchoPin, LED %ledPin"
     //% expandableArgumentMode="toggle"    
-    export function Circuit_2(potPin: AnalogPin, ledPin: AnalogPin, ldrPin: AnalogPin) {
+    export function Circuit_2(sonarTrigPin: DigitalPin, sonarEchoPin: DigitalPin, ledPin: DigitalPin) {
+        // This circuit takes the distance from a point as base line
+        // If distance is reduced, then something has tripped the 'beam'
+        // Sound alarm
 
+    }
+
+    function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d;
+        }
+    }
+
+    function checkMode(modes: number) {
+        input.onButtonPressed(Button.A, function () {
+            mode -= 1
+            if (mode <= 0) {
+                mode = modes
+            }
+            basic.showNumber(mode)
+        })
+        input.onButtonPressed(Button.B, function () {
+            mode += 1
+            if (mode >= modes) {
+                mode = 1
+            }
+            basic.showNumber(mode)
+        })
     }
 }
